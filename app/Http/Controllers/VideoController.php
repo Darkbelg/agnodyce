1b14bd31c58e99295ee08dc6a30192fcf40b28de
Darkbelg <stijn.sagaert@outlook.com>
2023-05-01 19:35:18 +0200

    Add singleton and DI to simplefy calling api

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SearchRequest;
 use App\Http\Requests\VideoRequest;
-use App\Service\Youtube\Search;
-use App\Service\Youtube\Videos;
 use App\Services\YoutubeAPI;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
@@ -21,20 +19,20 @@ class VideoController extends Controller
      * @param VideoRequest $request
      * @return Response|JsonResponse
      */
-    public function index(SearchRequest $request)
+    public function index(SearchRequest $request, YoutubeAPI $youtubeAPI)
     {
         $searchResults = Cache::remember(Hash::make(implode($request->validated())), 60 * 5,
-            function () use ($request) {
-                return (new YoutubeAPI())->search->listSearch('id,snippet', $request->validated());
+            function () use ($request, $youtubeAPI) {
+                return $youtubeAPI->search->listSearch('id,snippet', $request->validated());
             });
         return response()->json($searchResults);
     }
 
-    public function show(Request $request, $videoId)
+    public function show(Request $request, $videoId, YoutubeAPI $youtubeAPI)
     {
         $videoDetails = Cache::remember($videoId, 60 * 5,
-            function () use ($videoId) {
-                return (new YoutubeAPI())->videos->listVideos('statistics', ['id' => $videoId]);
+            function () use ($videoId, $youtubeAPI) {
+                return $youtubeAPI->videos->listVideos('statistics', ['id' => $videoId]);
             });
 
         return response()->json($videoDetails->items[0]->statistics);
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Service\Youtube\Connection;
+use App\Service\YoutubeAPI;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->singleton(Connection::class, function () {
             return new Connection();
+        });
+
+        $this->app->singleton(YoutubeAPI::class, function ($app) {
+            return new YoutubeAPI();
         });
     }
 
